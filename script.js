e694cd393131c4b6974fe0bbcb5ee3d0ee921906
Lucas pimentel <lucas47b@hotmail.com>
2026-05-14 11:34:18 -0300

    placar e telas de vitoria e derrota

--- script.js
+++ script.js
@@ -4,20 +4,42 @@ const papel = document.querySelector("#papel");
 const tesoura = document.querySelector("#tesoura");
 const sorteio = document.querySelector("#sorteio");
 const play = document.querySelector(".play");
+const novamente = document.querySelector("#novamente");
 const container = document.querySelector("#container");
 const jogar = document.querySelector("#jogar");
+const placar = document.querySelector("#placar")
 
 const jogardorParagrafo = document.querySelector("#jogador");
 const computadorParagrafo = document.querySelector("#computador");
-
+const resultadoFinal = document.querySelector("#resultadoFinal");
+const resultadoPartida = document.querySelector("#resultadoPartida");
 const resultadoTitulo = document.querySelector("#resultado");
-
+const jogadorPontos = document.querySelector("#jogadorPontos")
+const computadorPontos = document.querySelector("#computadorPontos");
 let jogadormao
 
+let plcarJogador = 0;
+let placarComputador = 0;
 
-play.addEventListener("click",()=>{
+play.addEventListener("click", () => {
     container.classList.toggle("esconder");
     jogar.classList.toggle("esconder");
+    placar.classList.toggle("esconder");
+
+});
+
+novamente.addEventListener("click", () => {
+    jogar.classList.toggle("esconder");
+
+    plcarJogador = plcarJogador - plcarJogador;
+    placarComputador = placarComputador - placarComputador;
+    computadorPontos.innerHTML = placarComputador;
+    jogadorPontos.innerHTML = plcarJogador;
+    resultadoFinal.classList.toggle("esconder");
+
+
+    
+
 });
 
 
@@ -32,11 +54,11 @@ sorteio.addEventListener("click", (e) => {
 const sorteioFuncao = () => {
 
 
-    let mao = ["👊","🖐","✌"];
+    let mao = ["👊", "🖐", "✌"];
 
     let random = Math.floor(Math.random() * 3);
 
-  
+
     let computadorMao = mao[random];
 
 
@@ -44,17 +66,17 @@ const sorteioFuncao = () => {
     if (jogador != undefined) {
         console.log(random)
         console.log("------------------------")
-        console.log("jogador: " +jogadormao)
+        console.log("jogador: " + jogadormao)
         console.log("computador:" + computador)
 
         jogardorParagrafo.innerHTML = jogadormao;
         computadorParagrafo.innerHTML = computadorMao;
-        if(jogadormao == undefined){
-           
+        if (jogadormao == undefined) {
+
             jogardorParagrafo.innerHTML = "";
             computadorParagrafo.innerHTML = "";
             resultadoTitulo.innerHTML = "Escolha um objeto"
-            
+
         }
         else if (jogadormao == computadorMao) {
             resultadoTitulo.innerHTML = "😑<br>EMPATE"
@@ -63,20 +85,49 @@ const sorteioFuncao = () => {
         } else if (jogadormao == "✌" && computadorMao == "👊") {
             resultadoTitulo.innerHTML = "😔<br>VOCÊ PERDEU!"
             console.log("VOCÊ PERDEU !!!");
+            placarComputador++
+            computadorPontos.innerHTML = placarComputador;
 
         } else if (jogadormao == "👊" && computadorMao == "🖐") {
             resultadoTitulo.innerHTML = "😔<br>VOCÊ PERDEU!"
             console.log("VOCÊ PERDEU !!!");
+            placarComputador++
+            computadorPontos.innerHTML = placarComputador;
 
         } else if (jogadormao == "🖐" && computadorMao == "✌") {
             resultadoTitulo.innerHTML = "😔<br>VOCÊ PERDEU!"
             console.log("VOCÊ PERDEU !!!");
+            placarComputador++
+            computadorPontos.innerHTML = placarComputador;
 
         } else {
-            resultadoTitulo.innerHTML = "😀<br>🎊VITÓRIA🎊"
+            resultadoTitulo.innerHTML = "😀<br>GANHOU ESSA"
             console.log("😀<br>VITóRIA !!!")
+            plcarJogador++
+            jogadorPontos.innerHTML = plcarJogador;
+
+
+
         }
 
+        if (plcarJogador == 5) {
+            resultadoPartida.innerHTML = "🤩<br>🎊Parabéns🎊<br>Você Ganhou <br> o Jogo!"
+            container.classList.add("esconder");
+            jogar.classList.add("esconder");
+            placar.classList.add("esconder");
+            resultadoFinal.classList.toggle("esconder");
+
+
+
+        } else if (placarComputador == 5) {
+            resultadoPartida.innerHTML = "😥<br> Você perdeu o jogo"
+            container.classList.add("esconder");
+            jogar.classList.add("esconder");
+            placar.classList.add("esconder");
+            resultadoFinal.classList.toggle("esconder");
+
+
+        }
 
 
     } else {
@@ -94,8 +145,8 @@ pedra.addEventListener("click", (e) => {
 
     jogador = "pedra";
     jogadormao = "👊"
-     jogardorParagrafo.innerHTML = jogadormao;
-    console.log(" Escolheu "+jogador)
+    jogardorParagrafo.innerHTML = jogadormao;
+    console.log(" Escolheu " + jogador)
     computadorParagrafo.innerHTML = "";
 });
 
@@ -105,7 +156,7 @@ papel.addEventListener("click", (e) => {
     jogador = "papel";
     jogadormao = "🖐"
     jogardorParagrafo.innerHTML = jogadormao;
-    console.log(" Escolheu "+jogador)
+    console.log(" Escolheu " + jogador)
     computadorParagrafo.innerHTML = "";
 });
 
@@ -115,6 +166,6 @@ tesoura.addEventListener("click", (e) => {
     jogador = "tesoura";
     jogadormao = "✌"
     jogardorParagrafo.innerHTML = jogadormao;
-    console.log(" Escolheu "+jogador)
+    console.log(" Escolheu " + jogador)
     computadorParagrafo.innerHTML = "";
 });
